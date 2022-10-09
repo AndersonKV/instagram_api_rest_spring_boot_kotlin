5d7ae2eadcaef3b76e5fda9c2c83d652025dc6a6
unknown <noitegarden@gmail.com>
2022-10-09 10:12:49 -0300

    começando projeto

---
+++ src/test/kotlin/com/backend/instagramapi/InstagramApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.backend.instagramapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InstagramApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
